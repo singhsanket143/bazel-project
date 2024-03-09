b116879d5ee71ee6cd09ea963c88addcaec16cfc
Sanket Singh <singhsanket143@gmail.com>
2024-03-09 22:35:10 +0530

    added codes

---
+++ src/main/java/com/algocamp/project/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.com.algocamp.project;
+
+import main.java.com.algocamp.project.utils.MyUtil;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        MyUtil.getString();
+    }
+}
---
+++ src/main/java/com/algocamp/project/utils/MyUtil.java
@@ -0,0 +1,7 @@
+package main.java.com.algocamp.project.utils;
+
+public class MyUtil {
+    public static void getString() {
+        System.out.println("Some string");
+    }
+}
